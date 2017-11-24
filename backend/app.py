5f33540b982ab89a445804c212bb617f8702ec0e
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2017-11-24 23:10:36 +0200

    added cors headers

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ import json
 import csv
 from flask import Flask
 
+from cors import crossdomain
+
 
 app = Flask(__name__)
 
 
 @app.route("/angular_velocity.csv", )
+@crossdomain(origin='*')
 def angular_velocity():
     with open('data/angular_velocity.csv', 'r') as csvfile:
         data = csvfile.read()
