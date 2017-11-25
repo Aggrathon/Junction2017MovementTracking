2a9fcc20865225ec4c5d9d90900c4909ef60469d
Aggrathon <antonbjo@gmail.com>
2017-11-25 13:44:44 +0200

    Data reading placeholder

--- ai/data.py
+++ ai/data.py
@@ -8,7 +8,25 @@ def get_data():
     """
         Get data samples for training
     """
-    #TODO read files
-    #TODO find footsteps
-    #TODO create sequences
-    return [(np.zeros([200, 12], np.float), np.zeros([PREDS], np.float)) for _ in range(100)]
+    data = [] 
+    for file in []: #TODO read files
+        f, label = read_file(file)
+        start = get_next_step(f, 0)
+        stop = get_next_step(f, start)
+        while stop > 0:
+            data.append((f[start:stop], label)
+            start = stop
+    data = [(np.zeros([200, 12], np.float), np.zeros([PREDS], np.float)) for _ in range(100)] #TODO remove
+    return data
+
+def get_next_step(data, start):
+    """
+        Find the next footstep
+    """
+    for i in range(start, len(data)):
+        if data[i]: #TODO find footstep
+            return i
+    return -1
+
+def read_file(file):
+    return [], [] #TODO read data
