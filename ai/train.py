43c8461b923de2aadd00cd907e854c7c35d540ed
Aggrathon <antonbjo@gmail.com>
2017-11-25 11:42:44 +0200

    Data input procedure

--- ai/train.py
+++ ai/train.py
@@ -7,7 +7,11 @@ import numpy as np
 from model import network, BATCH, PREDS, STEPS
 
 def input_gen():
-    yield np.zeros([STEPS, 12], np.float), np.zeros([PREDS], np.float)
+    #TODO read files
+    #TODO find footsteps
+    #TODO create sequences
+    while True:
+        yield np.zeros([STEPS, 12], np.float), np.zeros([PREDS], np.float)
 
 def input_fn():
     data = tf.data.Dataset.from_generator(input_gen, (tf.float32, tf.float32), (tf.TensorShape([STEPS, 12]), tf.TensorShape([PREDS])))
