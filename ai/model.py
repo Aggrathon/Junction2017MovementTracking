54ce608b7ebc7190b4d479099e7d5dbe54cd17d2
Aggrathon <antonbjo@gmail.com>
2017-11-25 10:50:22 +0200

    ai training

--- ai/model.py
+++ ai/model.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 BATCH = 32
 PREDS = 4
+STEPS = 100
 
 def model_fn(features, labels, mode):
     data = features['data']
@@ -13,31 +14,30 @@ def model_fn(features, labels, mode):
     lstm = tf.nn.rnn_cell.MultiRNNCell([
         tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.LSTMCell(size), dropout, dropout)
         for size in [30, 30 , 30, 30, 30]])
-    state = lstm.zero_state(batch_size, tf.float32)
-    for i in range(steps):
-        output, state = lstm(data[:,i], state)
-    output = tf.layers.dense(output, [BATCH, PREDS], activation=None)
+    state = lstm.zero_state(BATCH, tf.float32)
+    for i in range(STEPS):
+        output, state = lstm(data[:,i,:], state)
+    output = tf.layers.dense(output, PREDS, activation=None, name="logits")
     if mode != tf.estimator.ModeKeys.PREDICT:
         with tf.variable_scope("loss"):
             loss = tf.losses.softmax_cross_entropy(labels['pred'], output)
-            train = tf.trainer.AdamOptimizer().minimize(loss, tf.train.get_global_step())
+            train = tf.train.AdamOptimizer().minimize(loss, tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(
                 mode=mode,
-                predictions={'output': tf.softmax(output)},
+                predictions={'output': tf.nn.softmax(output)},
                 loss=loss,
                 train_op=train
         )
     else:
         return tf.estimator.EstimatorSpec(
             mode=mode,
-            predictions={'output': tf.softmax(output)}
+            predictions={'output': tf.nn.softmax(output)}
         )
-    
-    
+
+
 def network():
     return tf.estimator.Estimator(model_fn, 'network')
-        
+    
 
 if __name__ == "__main__":
     network()
-
