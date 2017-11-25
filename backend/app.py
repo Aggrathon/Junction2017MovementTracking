e407b82428c47d518adac33209b7cd1f739564f3
Jonas Hellman <jonas97_hellman@hotmail.com>
2017-11-25 18:41:52 +0200

    modified backend to recieve and parse data from app

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,37 @@
+#pylint: disable=C0111,W0603,C0103
 import json
-import csv
-from flask import Flask
-
-from cors import crossdomain
-
+import time
+from flask import Flask, request
 
 app = Flask(__name__)
 
+CAPTURE = None
 
-@app.route("/angular_velocity.csv", )
-@crossdomain(origin='*')
-def angular_velocity():
-    with open('data/angular_velocity.csv', 'r') as csvfile:
-        data = csvfile.read()
-    return data
 
+@app.route('/<command>')
+def start(command):
+    global CAPTURE
+    if command == 'start':
+        CAPTURE = ''
+    if command == 'stop':
+        with open('data.csv', 'w') as data_file:
+            data_file.write(CAPTURE)
+        CAPTURE = None
+
+
+@app.route('/', methods=["POST"])
+def index():
+    global CAPTURE
+
+    content = request.form
+    data = json.loads(content["data"])
+    timestamp = str(time.time())
+    coords = data['Body']['ArrayAcc'][0]
+    csv_line = [timestamp, coords['x'], coords['y'], coords['z']]
+    if CAPTURE:
+        CAPTURE += ','.join(csv_line) + '\n'
+
+    return ','.join(csv_line) + '\n'
 
 if __name__ == '__main__':
-    app.run(port=8000)
-
+    app.run(host='0.0.0.0')
