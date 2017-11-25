c203b0d3d99edf7442531f5ceb8bb38f7a52422b
Aggrathon <antonbjo@gmail.com>
2017-11-25 23:28:37 +0200

    simpler network

--- ai/model.py
+++ ai/model.py
@@ -36,9 +36,9 @@ def model_fn(features, labels, mode):
 
 def model2_fn(features, labels, mode):
     prev_layer = features['data']
-    prev_layer = tf.reshape(prev_layer, [BATCH, -1])
+    prev_layer = tf.reshape(prev_layer, [-1, 12*STEPS])
     dropout = 0.7 if tf.estimator.ModeKeys.TRAIN else 1.0
-    for i in [1024, 512, 128, 32]:
+    for i in [512, 512, 128, 32]:
         prev_layer = tf.layers.dense(prev_layer, i, activation=tf.nn.relu)
         prev_layer = tf.layers.dropout(prev_layer, dropout)
     output = tf.layers.dense(prev_layer, PREDS, activation=None, name="logits")
