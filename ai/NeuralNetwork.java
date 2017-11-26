b5b15c6f67d2d30bcb14a3884af5ea79ea18c209
Aggrathon <antonbjo@gmail.com>
2017-11-26 08:29:41 +0200

    Add python example to java

--- ai/NeuralNetwork.java
+++ ai/NeuralNetwork.java
@@ -41,4 +41,82 @@ public class NeuralNetwork implements IDisposable {
 			return null;
 		}
 	}
+
+	public bool addData(String name, float x, float y, float z) {
+		/*
+names = ['174630000602/Meas/Acc/13', '174630000602/Meas/Gyro/13', '174430000262/Meas/Acc/13', '174430000262/Meas/Gyro/13'],
+generator => yield: name, time, x, y, z 
+def combine(generator, names, file):
+    try:
+        data = []
+        pos = {n:i for i, n in enumerate(names)}
+        num = 0.0
+        post = [None for _ in names]
+        times = 0
+        for name, time, x, y, z in generator():
+            num += 1
+            times += time
+            post[pos[name]] = [x, y, z]
+            fin = True
+            for p in post:
+                if p is None:
+                    fin = False
+                    break
+            if fin:
+                line = [times/num]
+                times = 0
+                num = 0
+                for p in post:
+                    for o in p:
+                        line.append(o)
+                post = [None for _ in names]
+                data.append(line)
+        with open(file, 'w') as f:
+            f.write('time,'+','.join(n+'/x,'+n+'/y,'+n+'/z,' for n in names)+'\n')
+            for d in data:
+                f.write(','.join(map(str, d))+'\n')
+    except:
+		pass
+	
+
+smooth = data_smooth(data)
+start = get_next_step(smooth, 0)
+stop = get_next_step(smooth, start)
+while start > 0 and stop > 0:
+	data_all.append((data[start-1:stop+1], label))
+	start = stop
+	stop = get_next_step(smooth, start)
+
+def get_next_step(smooth, start):
+    """
+        Find the next footstep
+    """
+    start = max(start+2, 3)
+    for i in range(start, len(smooth)-9):
+        grt = True
+        for j in range(-3, 8):
+            if smooth[i] < smooth[i+j]:
+                grt = False
+                break
+        if grt:
+            return i
+	return -1
+	
+
+def data_abs(data):
+    abs = np.abs(data[:, 0])
+    for i in range(0, min(4, data.shape[1])):
+        abs += np.abs(data[:, i])
+    return abs
+
+def data_smooth(data):
+    abs = data_abs(data)
+    smooth = abs[0:-6]+abs[1:-5]*2+abs[2:-4]*4+abs[3:-3]*4+abs[4:-2]*2+abs[5:-1]
+    smooth = np.concatenate(([0,0,0], smooth, [0,0,0]))
+    return smooth
+
+
+		*/
+		return false; //A step beginning and end found?
+	}
 }
